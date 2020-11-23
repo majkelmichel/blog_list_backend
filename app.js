ca09157387779dc438bc11e997d0fa2a02369fb0
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-23 20:07:11 +0100

    ex. 4.1 and 4.2 done: restructured whole app

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const mongoose = require('mongoose');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+	.then(() => {
+		logger.info('connected');
+	})
+	.catch(err => {
+		logger.error('failed to connect:', err.message);
+	})
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
