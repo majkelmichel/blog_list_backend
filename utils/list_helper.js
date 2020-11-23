dedce8490e142ba955d4112ed96098e070d921d9
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-23 20:46:01 +0100

    Ex. 4.4 done: totalLikes tests working

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
 	return 1;
 }
 
+const totalLikes = (blogs) => {
+	const reducer = (sum, blog) => {
+		return sum + blog.likes;
+	}
+
+	return blogs.reduce(reducer, 0);
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
