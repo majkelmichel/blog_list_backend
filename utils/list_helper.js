529ef81488c9a15b8ee2f7f89287ab003be66b8d
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-23 20:56:48 +0100

    Ex. 4.5 done: written working tests for favorite blog checking

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(reducer, 0);
 }
 
+const favoriteBlog = (blogs) => {
+	const reducer = (top, blog) => {
+		if (blog.likes > top.likes) {
+			return blog;
+		}
+		return top;
+	}
+	const topBlog = blogs.reduce(reducer, {likes: -1});
+	return topBlog.likes !== -1
+	? {
+		title: topBlog.title,
+		author: topBlog.author,
+		likes: topBlog.likes
+	}
+	: null
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
